2e123e789fbbf1bde96c478ca5a70111169d99f1
Lyudmil Nikolov <nikolovlyudmill@gmail.com>
2023-12-30 13:23:36 +0200

    useHash strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,12 @@ import { GoogleLoginProvider, SocialAuthServiceConfig } from '@abacritt/angularx
 import { provideHttpClient } from "@angular/common/http";
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),
+  providers: [
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(),
     provideAnimations(),
     {
